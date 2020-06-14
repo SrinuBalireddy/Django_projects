76451ab314395423b19308eace8ddd4ac5d6e061
Srinu <srinuuipath@gmail.com>
2020-06-14 15:41:55 +0100

    created snippets app

--- rest_tutorial/rest_tutorial/settings.py
+++ rest_tutorial/rest_tutorial/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framwork',
+    'snippets.apps.SnippetConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
